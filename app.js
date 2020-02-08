a0e781dbc97d2a07840520d806b33e0cd4ee5f2f
mxcodemexico <mxcodemexico@gmail.com>
2020-02-08 11:49:37 -0600

    Maquetacion terminada

---
+++ app.js
@@ -0,0 +1,7 @@
+const sobreMi={
+    primero:'Soy de sistemas computacionales',
+    segundo:'Me gusta programar'
+}
+
+document.getElementById('primero').innerHTML=sobreMi.primero
+document.getElementById('segundo').innerHTML=sobreMi.segundo
